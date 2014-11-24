94f51187adb15de04ba474fcf4dded254b32280f
Neto <joseneto.design@gmail.com>
2014-11-24 12:50:29 -0200

    Teste com Angular

--- Prototipos/Angular Mat/mat.js
+++ Prototipos/Angular Mat/mat.js
@@ -1,4 +1,28 @@
-angular.module('materialDesign', ['ngMaterial'])
-    .controller('materialDesignControler', function($scope) {
+angular.module('textFieldDemo1', ['ngMaterial'])
 
-    });
+/**
+ *  Simple controller to build a `user` data model
+ *  that will be used to databinding with `<tf-float>` directives
+ */
+.controller('DemoController', function($scope) {
+    $scope.listaAcomodacoes = [{
+        titulo: 'Quarto 1',
+        disponibilidade: "desocupado"
+    }, {
+        titulo: 'Quarto 2',
+        disponibilidade: "ocupado"
+    }, {
+        titulo: 'Quarto 3',
+        disponibilidade: "desocupado"
+    }];
+
+
+    $scope.mudar = function(acomodacao, desocupar) {
+        if (desocupar)
+            acomodacao.disponibilidade = "desocupado";
+        else
+            acomodacao.disponibilidade = "ocupado";
+    }
+
+
+})
