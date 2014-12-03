dca5a0bd2537b6bab9f973d2ceb6369eb372e668
Neto <joseneto.design@gmail.com>
2014-12-03 11:24:39 -0200

    server

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-var errorHandler = require('errorhandler');
-var methodOverride = require('method-override');
-var port = parseInt(process.env.PORT, 10) || 8080;
+
+var port = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
   res.redirect("/hello.html");
 });
 
-app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static(__dirname + '/fw'));
-app.use(errorHandler({
-  dumpExceptions: true,
-  showStack: true
-}));
+app.use(express.static(__dirname + '/framework'));
 
-console.log("Iniciando servidor HTTP em http://localhost:" + port);
+console.log("Iniciando servidor HTTP na porta " + port);
 app.listen(port);
-
-
