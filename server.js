69d6653824e51e14cfafc0ba0d801d5657346d4a
Neto <joseneto.design@gmail.com>
2014-12-03 11:26:29 -0200

    mudanca de nome

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.get("/", function (req, res) {
-  res.redirect("/hello.html");
+app.get("/", function(req, res) {
+    res.redirect("/hello.html");
 });
 
-app.use(express.static(__dirname + '/framework'));
+app.use(express.static(__dirname + '/www'));
 
 console.log("Iniciando servidor HTTP na porta " + port);
 app.listen(port);
